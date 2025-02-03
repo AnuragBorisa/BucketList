103d757fbba5a8cdfd08d793d83c667b2aa6a254
Anurag Borisa <anuragborisa@gmail.com>
2025-02-03 23:20:11 +0530

    "Added locations"

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,16 +6,58 @@
 //
 
 import SwiftUI
+import MapKit
+import LocalAuthentication
+
+
 
 struct ContentView: View {
+    @State private var locations = [Location]()
+    @State private var selectedPlace : Location?
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center:CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span:MKCoordinateSpan(latitudeDelta: 10, longitudeDelta:10)
+            
+        )
+    )
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader {proxy in
+            Map(initialPosition: startPosition){
+                ForEach(locations){location in
+                    Annotation(location.name,coordinate: location.coordinate){
+                        Image(systemName:"star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width:44,height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture{
+                                selectedPlace = location
+                            }
+                    }
+                }
+            }
+                .onTapGesture{position in
+                    if let coordinate = proxy.convert(position,from: .local){
+                        print("Tapped on \(coordinate)")
+                        print("Location struct,\(Location.self)")
+                        let newLocation = Location(id:UUID(),name:"New Location",description:"",latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        locations.append(newLocation);
+                    }
+                }
+                .mapStyle(.hybrid)
+                .sheet(item:$selectedPlace){place in
+                    EditView(location: place){
+                        newLocation in
+                        if let index = locations.firstIndex(of:place){
+                            locations[index] = newLocation
+                        }
+                    }
+                }
         }
-        .padding()
+       
     }
 }
 
