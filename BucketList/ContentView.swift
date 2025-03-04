b17c58199ecc52538cb5e3214460206064f8bf79
Anurag Borisa <anuragborisa@gmail.com>
2025-03-04 12:12:35 +0530

    introduced MVVM arch

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -1,66 +1,63 @@
-//
-//  ContentView.swift
-//  BucketList
-//
-//  Created by Anurag on 01/02/25.
-//
+    //
+    //  ContentView.swift
+    //  BucketList
+    //
+    //  Created by Anurag on 01/02/25.
+    //
 
-import SwiftUI
-import MapKit
-import LocalAuthentication
+    import SwiftUI
+    import MapKit
+    import LocalAuthentication
 
+   
 
-
-struct ContentView: View {
-    @State private var locations = [Location]()
-    @State private var selectedPlace : Location?
-    
-    let startPosition = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center:CLLocationCoordinate2D(latitude: 56, longitude: -3),
-            span:MKCoordinateSpan(latitudeDelta: 10, longitudeDelta:10)
-            
+    struct ContentView: View {
+        let startPosition = MapCameraPosition.region(
+            MKCoordinateRegion(
+                center:CLLocationCoordinate2D(latitude: 56, longitude: -3),
+                span:MKCoordinateSpan(latitudeDelta: 10, longitudeDelta:10)
+                
+            )
         )
-    )
-    var body: some View {
-        MapReader {proxy in
-            Map(initialPosition: startPosition){
-                ForEach(locations){location in
-                    Annotation(location.name,coordinate: location.coordinate){
-                        Image(systemName:"star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .frame(width:44,height: 44)
-                            .background(.white)
-                            .clipShape(.circle)
-                            .onLongPressGesture{
-                                selectedPlace = location
-                            }
-                    }
-                }
-            }
-                .onTapGesture{position in
-                    if let coordinate = proxy.convert(position,from: .local){
-                        print("Tapped on \(coordinate)")
-                        print("Location struct,\(Location.self)")
-                        let newLocation = Location(id:UUID(),name:"New Location",description:"",latitude: coordinate.latitude, longitude: coordinate.longitude)
-                        locations.append(newLocation);
-                    }
-                }
-                .mapStyle(.hybrid)
-                .sheet(item:$selectedPlace){place in
-                    EditView(location: place){
-                        newLocation in
-                        if let index = locations.firstIndex(of:place){
-                            locations[index] = newLocation
+        
+        @State private var viewModel = ViewModel()
+        
+        var body: some View {
+            MapReader {proxy in
+                Map(initialPosition: startPosition){
+                    ForEach(viewModel.locations){location in
+                        Annotation(location.name,coordinate: location.coordinate){
+                            Image(systemName:"star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width:44,height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture{
+                                    viewModel.selectedPlace = location
+                                }
                         }
                     }
                 }
+                    .onTapGesture{position in
+                        if let coordinate = proxy.convert(position,from: .local){
+                            print("Tapped on \(coordinate)")
+                            print("Location struct,\(Location.self)")
+                            viewModel.addLocation(at: coordinate)
+                        }
+                    }
+                    .mapStyle(.hybrid)
+                    .sheet(item:$viewModel.selectedPlace){place in
+                        EditView(location: place){
+                            newLocation in
+                            viewModel.update(location: newLocation)
+                        }
+                    }
+            }
+           
         }
-       
     }
-}
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
